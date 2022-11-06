8d7ece7accc7ba233de6d9bdc096e5aaacab275c
IceTeaXXD <13521024@std.stei.itb.ac.id>
2022-11-06 20:02:23 +0700

    Fixed RataRataMatrix

--- src/OperasiMatriks.py
+++ src/OperasiMatriks.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def SumOfMatrix(M1, M):
-    for i in range(M1):
-        for j in range(M1):
+    for i in range(256):
+        for j in range(256):
             M[i][j] += M1[i][j]
 
 def RataRataMatrix(S):
     M = len(S)
     sumofmatriks = [[0 for j in range(256)] for i in range(256)]
     for matriks in S:
-        SumOfMatrix(sumofmatriks,matriks)
+        SumOfMatrix(matriks, sumofmatriks)
     for i in range (256):
         for j in range(256):
             sumofmatriks[i][j] /= M
