bb56a97ee353a7864d7bd661a83730e4448ae206
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-11-06 21:28:56 +0700

    Eigen Face draft

--- src/EigenFace.py
+++ src/EigenFace.py
@@ -1,2 +1,11 @@
 # filename: EigenFace.py
 # deskripsi: melakukan algoritma eigenface
+
+def EigenFace(eigenvector, selisih, S):
+    #I.S. eigenvector dan selisih berupa matriks terdefinisi.
+            # S set of matriks
+    #F.S. mengembalikan nilai eigenface dari satu wajah
+    miu = 0
+    for i in range(0,len(S)):
+        miu += selisih[i] * eigenvector[0][i]
+    return miu
