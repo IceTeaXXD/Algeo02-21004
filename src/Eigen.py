375999eecefc92b5acfe4182cb179c293ac0b285
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-11-08 12:17:19 +0700

    Added A to R,Q and get Eigen values. A to Q still draft

--- src/Eigen.py
+++ src/Eigen.py
@@ -1,5 +1,6 @@
 # filename: Eigen.py
 # deskripsi: melakukan operasi-operasi eigen
+import numpy as np
 
 def VektorSatuan(M):
     sum = 0
@@ -13,3 +14,38 @@ def DotProduct(M,N):
     for i in range (256):
         sum = sum + M[i] * N[i]
     return sum
+
+def GetVectorK(matriks, k):
+    temp = []
+    for i in range(256):
+        for j in range(256):
+            if (j == k):
+                temp.extend(matriks[i][j])
+    return temp
+
+def A_to_Q(matriks):
+    # buat matriks 256 x 256
+    Q = [[0 for i in range (256)] for j in range(256)]
+    Q = np.array(Q)
+    for i in range(256):
+        temp = GetVectorK(matriks,i)
+                    
+    return Q
+
+def A_to_R(Q, A):
+    # Make transpose
+    temp = Q
+    for i in range(256):
+        for j in range(256):
+            Q[i][j] = temp[j][i]
+    R = np.dot(Q,A)
+    return R
+
+def getEigen(A):
+    # iterasi sebanyak 20 kali saja
+    EigenVal = A
+    for i in range(20):
+        Q = A_to_Q(EigenVal)
+        R = A_to_R(EigenVal)
+        EigenVal = np.dot(R,Q)
+    return EigenVal
