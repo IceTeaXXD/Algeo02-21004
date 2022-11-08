81584f6a47164535ce320938c19318c01089505c
henryanandsr <henryanandseptianradityo@gmail.com>
2022-11-08 12:57:44 +0700

    add matrix Q

--- src/Eigen.py
+++ src/Eigen.py
@@ -5,9 +5,9 @@ import numpy as np
 def VektorSatuan(M):
     sum = 0
     for i in range (256):
-        sum = M[0][i] * M[0][i]
+        sum = M[i] * M[i]
     for i in range (256):
-        M[0][i] = M[0][i] / sum
+        M[i] = M[i] / sum
 
 def DotProduct(M,N):
     sum = 0
@@ -21,6 +21,7 @@ def GetVectorK(matriks, k):
         for j in range(256):
             if (j == k):
                 temp.extend(matriks[i][j])
+    temp = np.array(temp)
     return temp
 
 def A_to_Q(matriks):
@@ -28,8 +29,17 @@ def A_to_Q(matriks):
     Q = [[0 for i in range (256)] for j in range(256)]
     Q = np.array(Q)
     for i in range(256):
+        ak = GetVectorK(matriks,i)
         temp = GetVectorK(matriks,i)
-                    
+        if (i != 0):
+            VektorSatuan(temp)
+            Q.extend(temp)
+        else:
+            temp = ak
+            for j in range (i):
+                ej = getVector(Q,j)
+                temp +=  add(ak,np.dot(DotProduct(ak,ej),ej))
+            Q.extend(temp)
     return Q
 
 def A_to_R(Q, A):
