df5405e5ce132111fb906a931f99145f0c3a0343
henryanandsr <henryanandseptianradityo@gmail.com>
2022-11-08 11:36:54 +0700

    add DotProduct & VektorSatuan

--- src/Eigen.py
+++ src/Eigen.py
@@ -1,2 +1,15 @@
 # filename: Eigen.py
 # deskripsi: melakukan operasi-operasi eigen
+
+def VektorSatuan(M):
+    sum = 0
+    for i in range (256):
+        sum = M[0][i] * M[0][i]
+    for i in range (256):
+        M[0][i] = M[0][i] / sum
+
+def DotProduct(M,N):
+    sum = 0
+    for i in range (256):
+        sum = sum + M[i] * N[i]
+    return sum
