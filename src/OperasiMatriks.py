e6cb7eff56bbae8b0c97c1300f53f078526bc418
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-11-14 16:35:41 +0700

    GFG Vers

--- src/OperasiMatriks.py
+++ src/OperasiMatriks.py
@@ -1,33 +1,38 @@
 import numpy as np
 
 def SumOfMatrix(M1, M):
-    for i in range(256):
-        for j in range(256):
-            M[i][j] += M1[i][j]
+    for i in range(len(M[0])):
+        for j in range(len(M)):
+            M[j][i] += M1[j][i]
 
 def RataRataMatrix(S):
     M = len(S)
-    sumofmatriks = [[0 for j in range(256)] for i in range(256)]
+    sumofmatriks = [[0 for j in range(len(S[0][0]))] for i in range(len(S[0]))]
     for matriks in S:
         SumOfMatrix(matriks, sumofmatriks)
-    for i in range (256):
-        for j in range(256):
-            sumofmatriks[i][j] /= M
+    for i in range (len(sumofmatriks[0])):
+        for j in range(len(sumofmatriks)):
+            sumofmatriks[j][i] /= M
     return sumofmatriks
 
 #cari selisih
 def Selisih(S,data):
     sp = np.array(S)
-    d = np.array([[[0 for j in range (256)] for i in range (256)] for k in range (data)])
+    d = np.array([[[0 for j in range (len(S[0][0]))] for i in range (len(S[0]))] for k in range (data)])
     mp = np.array(RataRataMatrix(S))
     for i in range (data):
         d[i] = np.subtract(sp[i],mp)
     return d
 
 def kovarian(S,data):
-    k = np.array([[0 for i in range (256)] for j in range (256)])
     sp = np.array(S)
-    for i in range (data):
-        k = k + np.dot(sp[i],np.transpose(sp[i]))
-    k = np.multiply(k,(1/data))
+    k = np.array([S[0]])
+    # k = np.concatenate((k,[sp[1]]),axis = 0)
+    # k = np.concatenate((k,[sp[2]]),axis = 0)
+    # k = np.transpose(k)
+    for i in range (1,data):
+        k = np.concatenate((k,[sp[i]]),axis = 0) 
+    kt = np.transpose(k)
+    # c = np.dot(kt[0],k)
+    # k = np.transpose(k)
     return k
