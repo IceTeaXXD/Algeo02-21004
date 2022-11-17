abc2a282be5e4a63a60cf9ac7ced2b9a28411c57
Matthew Mahendra <matthew.mahendra@gmail.com>
2022-11-17 10:35:45 +0700

    Added Crop Function

--- src/cam.py
+++ src/cam.py
@@ -2,8 +2,10 @@ import cv2
 import time
 
 cam = cv2.VideoCapture(0)
-
+# cam.set(3,1080)
 cv2.namedWindow("Webcam")
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # img_counter = 0
 
@@ -24,18 +26,20 @@ while True:
         print("Escape hit, closing...")
         break
 
-    # elif k%256 == 32:
-    #     # SPACE pressed
-    #     img_name = "opencv_frame_{}.png".format(img_counter)
-    #     cv2.imwrite(img_name, frame)
-    #     print("{} written!".format(img_name))
-    #     img_counter += 1
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "Face.jpg"
+        # img = cv2.resize(frame, (256,256), interpolation=cv2.INTER_AREA)
+        # cv2.imwrite(img_name, img)
+        img = frame[0:480,80:560]
+        cv2.imwrite(img_name, img)
+        print("{} written!".format(img_name))
 
     # capture image every 10 seconds
-    if time.time() % 10 < 0.1:
-        img_name = "Face.jpg"
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+    # if time.time() % 10 < 0.1:
+    #     img_name = "Face.jpg"
+    #     cv2.imwrite(img_name, frame)
+    #     print("{} written!".format(img_name))
 
 cam.release()
 
