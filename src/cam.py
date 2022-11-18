6ef5e949c3b377837f18fe15b79ea13792675fbb
IceTeaXXD <13521024@std.stei.itb.ac.id>
2022-11-18 13:08:44 +0700

    Added face detection and capture

--- src/cam.py
+++ src/cam.py
@@ -1,46 +1,38 @@
 import cv2
 import time
 
-cam = cv2.VideoCapture(0)
-# cam.set(3,1080)
-cv2.namedWindow("Webcam")
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('face_detector.xml')
 
-# img_counter = 0
-
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+roi_color = None
 while True:
-    ret,frame = cam.read()
-
-    if not ret:
-        print("failed to grab frame")
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        # capture every 5 seconds
+        if time.time() % 5 < 0.1:
+            print("Image Captured!")
+            cv2.imwrite("face.jpg", roi_color)
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
         break
-
-    cv2.imshow("Webcam", frame)
-
-    k = cv2.waitKey(1)
+    # capture the detected face
 
 
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "Face.jpg"
-        # img = cv2.resize(frame, (256,256), interpolation=cv2.INTER_AREA)
-        # cv2.imwrite(img_name, img)
-        img = frame[0:480,80:560]
-        cv2.imwrite(img_name, img)
-        print("{} written!".format(img_name))
-
-    # capture image every 10 seconds
-    # if time.time() % 10 < 0.1:
-    #     img_name = "Face.jpg"
-    #     cv2.imwrite(img_name, frame)
-    #     print("{} written!".format(img_name))
-
-cam.release()
-
-cam.destroyAllWindows()
+# Release the VideoCapture object
+cap.release()
