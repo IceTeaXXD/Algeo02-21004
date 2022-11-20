c2da795f5e0285c4c6f39159d48bd9bc6bdff0c9
henryanandsr <henryanandseptianradityo@gmail.com>
2022-11-20 17:05:35 +0700

    Fix camera

--- src/main.py
+++ src/main.py
@@ -17,6 +17,63 @@ import imutils
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path(r"assets/frame0")
+def detectCam(dataset):
+    start = time.time()
+    path = "../face.jpg"
+    if (dataset!=None):
+        image = cv.imread(path)
+        image = cv.resize(image, (256,256))
+
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        image = ImageTk.PhotoImage(image)
+        S = II.DataSetToMatrix(dataset)
+        print("Done 1")
+
+        # Hitung rata-rata
+        mean = OM.RataRataMatrix(S)
+        print("Done 2")
+
+        # Hitung selisih
+        s2 = OM.Selisih(S, len(S))
+        print("Done 3")
+
+        # Buat Kovarian
+        cov = OM.kovarian(s2, len(s2))
+        print("Done 4")
+
+        # Hitung EigenVector dari Kovarian
+        eigenval, eigenvec = Eig.getEigen(cov)
+        print("Done 5")
+
+        # Hitung EigenFace training Images
+        eigface,weightf = EigF.EigenFace(eigenvec, s2, S)
+        print("Done 6")
+            
+        weightnf = EigF.EigenNewFace(path,mean,eigface)
+        print("Done 7")
+
+        idx,th = EigF.EuclideanDistance(weightf,weightnf)
+        print("Done 8")
+
+        if (th):
+            image_result = Image.fromarray(np.reshape(S[idx], (256,256)))
+            image_result = ImageTk.PhotoImage(image_result)
+            canvas.itemconfig(img_result,image = image_result)
+            label_result.image = image_result
+            canvas.itemconfig(name,text = "Result: ")
+        else:
+            image_result = Image.open("not found.png")
+            image_result = image_result.resize((256,256))
+            image_result = ImageTk.PhotoImage(image_result)
+            canvas.itemconfig(img_result,image = image_result)
+            label_result.image = image_result
+            canvas.itemconfig(name,text = "Not Found")
+        end = time.time()
+        t = end-start
+        canvas.itemconfig(wkt,text = "Compile time: "+ str(round(t,2)) + " second", font=("Poppins Bold", 20 * -1))
+    else:
+        canvas.itemconfig(name,text="Silakan pilih dataset terlebih dahulu!",font=("Poppins Bold", 20 * -1))
 
 def select_gambar(dataset):
     start = time.time()
@@ -119,7 +176,7 @@ canvas = Canvas(
 canvas.place(x = 0, y = 0)
 # camera = tk.scale
 # Gambar Input
-img = Image.open("D:/SemesterIII/Algeo/Tubes2/2/Algeo02-21004/src/default.jpg")
+img = Image.open("default.jpg")
 img = img.resize((256,256))
 img = ImageTk.PhotoImage(img)
 img_input = canvas.create_image(
@@ -197,11 +254,11 @@ button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=lambda: [Quit(),select_gambar(dataset)],
+    command=lambda: [select_gambar(dataset)],
     relief="flat"
-)   
+)
 button_1.place(
-    x=536.0,
+    x=676.0,
     y=569.0,
     width=198.06346130371094,
     height=35.0
@@ -214,7 +271,7 @@ def Stream():
     Cam()
 def Cam():
 # Load the cascade
-    face_cascade = cv.CascadeClassifier('D:/SemesterIII/Algeo/Tubes2/2/Algeo02-21004/src/face_detector.xml')
+    face_cascade = cv.CascadeClassifier('./face_detector.xml')
     global cap
     _, img = cap.read()
     tempimg = img
@@ -234,6 +291,7 @@ def Cam():
             # capture every 5 seconds
             if time.time() % 5 < 0.1:
                 print("Image Captured!")
+                detectCam(dataset)
                 cv.imwrite("face.jpg", roi_color)
         gambar = Image.fromarray(tempimg)
         rgambar = gambar.resize((250,250))
@@ -259,7 +317,7 @@ button_2 = Button(
     image=button_image_2,
     borderwidth=0,
     highlightthickness=0,
-    command= Stream,
+    command= lambda: [Stream()],
     relief="flat"
 )
 button_2.place(
@@ -283,6 +341,22 @@ button_3.place(
     y=569.0,
     width=220.0,
     height=33.0
+)
+button_image_4 = PhotoImage(
+    file = relative_to_assets("button_4.png")
+)
+button_4 = Button(
+    image = button_image_4,
+    borderwidth= 0,
+    highlightthickness=0,
+    command = Quit,
+    relief="flat"
+)
+button_4.place(
+    x = 360,
+    y = 569,
+    width = 175,
+    height = 42,
 )
 window.resizable(False, False)
 window.mainloop()
